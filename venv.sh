ca2a7ec7a6dc4c2a2d718cc2b4cae440187e208f
Kethan <k.ponnuthurai@student.han.nl>
2024-09-12 11:32:57 +0200

    updated the script file.

--- venv.sh
+++ venv.sh
@@ -2,6 +2,7 @@
 python3 -m venv .venv
 source .venv/bin/activate
 
+
 pip install numpy
 pip install scipy
 pip install ipython
@@ -9,3 +10,4 @@ pip install scikit-learn
 pip install pandas
 pip install mglearn
 pip install -U matplotlib
+pip install opencv-python
