0b175dcdf98625c447bf169aa499acf0ee5eab98
Kethan <k.ponnuthurai@student.han.nl>
2024-09-12 11:13:40 +0200

    Created a script for the virtual environment.

---
+++ venv.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install numpy
+pip install scipy
+pip install ipython
+pip install scikit-learn
+pip install pandas
+pip install mglearn
+pip install -U matplotlib
